5925449f127782feb3ea85707cf6917485627479
wayneLin <gn01964422@gmail.com>
2018-04-11 12:26:57 +0800

    安裝automapper和建立viewmodel

---
+++ DemoMVC/App_Start/MapperConfig.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using DemoMVC.Models;
+using DemoMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoMVC.App_Start
+{
+    public class MapperConfig
+    {
+        public static IMapper Mapper { get; set; }
+
+        public static void Initialize()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Member, MemberVM>();
+                cfg.CreateMap<MemberVM, Member>();
+             });
+
+            Mapper = config.CreateMapper();
+        }
+    }
+}
